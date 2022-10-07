c64ee62c735c90a44c2540f059de2c3f9a781358
Gautam Tiwari <gautamtiwari.dev@gmail.com>
2022-10-08 00:13:44 +0530

    Weather feature added

---
+++ components/Weather.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import styles from "./Weather.module.css";
+const Weather = ({ className, ...props }) => {
+    const [weather, setWeather] = useState({});
+    const getWeather = (lat, lon) => {
+        fetch(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=fa486f1b70e748f065b1ce342ffd0ff7`
+        )
+            .then((res) => res.json())
+            .then((res) => {
+                console.log(res);
+                setWeather(res);
+            })
+            .catch((err) => console.log(err));
+    };
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition((pos) => {
+            getWeather(pos.coords.latitude, pos.coords.longitude);
+        });
+    }, []);
+
+    return (
+        <div className={styles.container}>
+            <span className={styles.temp}>
+                {Math.round(weather.main?.temp - 273.15)}°
+            </span>
+            <span className={styles.city}>{weather.name} </span>
+            <span className={styles.weather}>
+                {weather.weather[0]?.description}
+            </span>
+        </div>
+    );
+};
+
+const MemoizedWeather = React.memo(Weather);
+export default MemoizedWeather;
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Script from "next/script";
 import Image from "next/future/image";
 import logo from "../public/logo.svg";
 import styles from "../styles/Home.module.css";
+import Weather from "../components/Weather";
 
 export default function Home() {
     return (
@@ -17,6 +18,8 @@ export default function Home() {
                 id="search-engine"
                 src="https://cse.google.com/cse.js?cx=f01b910a83b574d5c"
             ></Script>
+
+            <Weather className="weather"></Weather>
             <Image src={logo} alt="logo" className={styles.logo} />
             <div className="gcse">
                 <div className="gcse-searchbox"></div>
